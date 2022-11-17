310e16417a1b153b147a22c6a56d0d94d99e7a98
unknown <adrianpuerta18@gmail.com>
2022-11-17 18:46:04 -0500

    create blueprints

---
+++ blueprints/candidateBlueprints.py
@@ -0,0 +1,13 @@
+from flask import Blueprint
+from flask import request
+
+from controllers.candidateController import CandidateController
+
+candidate_blueprint = Blueprint('candidate_blueprint', __name__)
+candidate_controller = CandidateController()
+
+
+@candidate_blueprint.route("/candidate/all", methods=['GET'])
+def get_candidates():
+    response = candidate_controller.index();
+    return response, 200
