7d0ba2d7fd10be7c9a1a2c4b3ee6bb221c4f503e
unknown <adrianpuerta18@gmail.com>
2022-11-21 21:13:00 -0500

    create constructor from models

--- models/abstractModel.py
+++ models/abstractModel.py
@@ -3,6 +3,11 @@ from abc import ABCMeta
 
 class AbstractModel(metaclass=ABCMeta):
 
-    def __init__(self, data):
+    def __init__(self, data: dict):
+        """
+        this constructor is used to transform a dictionary call data
+        into an object making a flexible attributes definition
+        :param data: data to convert into object
+        """
         for key, value in data.items():
             setattr(self, key, value)
