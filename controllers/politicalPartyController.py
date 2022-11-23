41a92453c20a8ce4ed9a6818d938dce26418687f
unknown <adrianpuerta18@gmail.com>
2022-11-22 19:04:53 -0500

    create controllers political party

--- controllers/politicalPartyController.py
+++ controllers/politicalPartyController.py
@@ -4,22 +4,47 @@ from models.politicalParty import PoliticalParty
 class PoliticalPartyController:
 
     def __init__(self):
+        """
+        this is the constructor of the politicalPartyController class
+        """
         print("Political Party Controller ready")
 
-    # Equivalent to 'all'
-    def index(self):
+    def index(self) -> list:
+        """
+        this method returns all political parties persisted in the db
+        :return:
+        """
         print("return all Political Parties")
 
-    # Equivalent to 'one by id'
-    def show(self, id_):
+    def show(self, id_: str) -> dict:
+        """
+        this method return one political party persisted in the db
+        :param id_:
+        :return:
+        """
         print("return one Political Party")
 
-    # Equivalent to 'insert'
-    def create(self, politicalParty_):
+    def create(self, politicalParty_: dict) -> dict:
+        """
+        this method create one political party
+        :param politicalParty_:
+        :return:
+        """
         print("insert a Political Party")
 
-    def update(self, id_, politicalParty_):
+    def update(self, id_: str, politicalParty_: dict) -> dict:
+        """
+        update political party registration
+        :param id_:
+        :param politicalParty_:
+        :return:
+        """
         print("update an Political Party")
 
-    def delete(self, id_):
+    def delete(self, id_: str) -> str:
+        """
+        delete political party registration
+        :param id_:
+        :return:
+        """
         print("delete a Political Party")
