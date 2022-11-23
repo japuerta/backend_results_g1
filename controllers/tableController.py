b3fa40c60d37ea9e92d457d77a35f73e7957f26c
unknown <adrianpuerta18@gmail.com>
2022-11-22 19:05:37 -0500

    create controllers table

--- controllers/tableController.py
+++ controllers/tableController.py
@@ -4,22 +4,48 @@ from models.table import Table
 class TableController:
 
     def __init__(self):
+        """
+        this is the constructor of the TableController class
+        """
         print("Table Controller ready")
 
-    # Equivalent to 'all'
-    def index(self):
-         print("return all tables")
+    def index(self) -> list:
+        """
+        this method returns all tables persisted in the db
+        :return:
+        """
+        print("return all tables")
 
-    # Equivalent to 'one by id'
-    def show(self, id_):
+    def show(self, id_: str) -> dict:
+        """
+        this method return one table persisted in the db
+        :param self:
+        :param id_:
+        :return:
+        """
         print("return one table")
 
-    # Equivalent to 'insert'
-    def create(self, table_):
+    def create(self, table_: dict) -> dict:
+        """
+        this method create one table
+        :param table_:
+        :return:
+        """
         print("insert a table")
 
-    def update(self, id_, table_):
+    def update(self, id_: str, table_: dict) -> dict:
+        """
+        update table registration
+        :param id_:
+        :param table_:
+        :return:
+        """
         print("update an table")
 
-    def delete(self, id_):
+    def delete(self, id_: str) -> str:
+        """
+        delete table registration
+        :param id_:
+        :return:
+        """
         print("delete a table")
